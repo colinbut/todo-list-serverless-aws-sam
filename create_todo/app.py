7462134ba4e30818ae8e6fefbd9000d48bc9c12f
Colin But <colin.but@outlook.com>
2020-11-06 00:09:39 +0000

    feat: add create_todo lambda function

--- create_todo/app.py
+++ create_todo/app.py
@@ -1,12 +1,23 @@
 import json
 import boto3
+import uuid
 
 def lambda_handler(event, context):
-    print(event)
-    print(context)
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-        }),
-    }
+    print("event: {}".format(event))
+    print("context: {}".format(context))
+
+    print("Inserting todo: {} into database".format(event))
+
+    client = boto3.client('dynamodb')
+    client.put_item(
+        TableName='Todos',
+        Item={
+            "item_id": str(uuid.uuid1()),
+            "item_title": event.title,
+            "item_content": event.content
+        }
+    )
+
+    print("Inserted todo: {} into database".format(event))
+
+    return {"statusCode": 201}
