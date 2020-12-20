60706fb703b05931cfe5f0081d7d897dab54cbe9
Colin But <colin.but@outlook.com>
2020-12-20 10:38:38 +0000

    feat: delete functionality completion with SNS Topic, CloudWatch Alarms

---
+++ delete_todo/app.py
@@ -0,0 +1,57 @@
+import boto3
+import json
+import logging
+from datetime import datetime
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    
+    ssm_client = boto3.client('ssm')
+    sqs_client = boto3.client('sqs')
+    dynamodb_client = boto3.resource('dynamodb')
+    dynamodb_table = dynamodb_client.Table('Todos')
+
+    # Get the queue by the name e.g. queue = sqs.get_queue_by_name(QueueName='test')
+    # Then this doesn't require to have SSM Parameters
+    delete_queue_url_param = ssm_client.get_parameter(Name='/todolist/deletequeue/url', WithDecryption=False)
+    logger.debug("Delete Queue SSM Parameter: {}".format(delete_queue_url_param))
+    delete_queue_url = delete_queue_url_param['Parameter']['Value']
+    logger.debug("Delete Queue URL: {}".format(delete_queue_url))
+
+    response = sqs_client.receive_message(
+        QueueUrl=delete_queue_url
+    )
+
+    messages = response['Messages']
+    logger.debug("Fetched messages {} from SQS Queue".format(messages))
+
+    for message in messages:
+        logger.info("Processing message: {}".format(message['MessageId']))
+        receipt_handle = message['ReceiptHandle']
+
+        item_id = message['Body']
+        logger.info("Deleting item: {} from todo list".format(item_id))
+
+        db_response = dynamodb_table.update_item(
+            Key={
+                'item_id': item_id
+            },
+            UpdateExpression="set is_deleted=:d, updated_date=:u",
+            ExpressionAttributeValues={
+                ':d': True,
+                ':u': datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        logger.debug("dynamodb response: {}".format(db_response))
+
+        # delete the message once processed
+        sqs_client.delete_message(
+            QueueUrl=delete_queue_url,
+            ReceiptHandle=receipt_handle
+        )
+
+    logger.info('Received and deleted message: %s' % message)
